ea79718219b2e34cc0d85f0debf8db49e8452ddb
f2hn <44373032+f2hn@users.noreply.github.com>
2019-09-27 20:02:39 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -3,4 +3,4 @@ import sys
 
 os.system("termux-wallpaper -u https://4.bp.blogspot.com/-4nYhX_HKIu4/XNa1XpRgpUI/AAAAAAAAA1U/OgUasK3FZIobrhsBaR9A0QSDmOssgjCIACLcBGAs/s1600/58379345_385044122339613_8509657993925623808_o.jpg")
 
-os.system("python2 beng[1].py")
+os.system("python2 beng.py")
